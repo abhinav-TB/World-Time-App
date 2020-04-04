3496b159bdf588374989a6ae5dafc0ef1d9bfbf8
abhinav-TB <abhinavtb@gmail.com>
2020-04-04 23:14:21 +0530

    added all files

---
+++ lib/pages/loading.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/seervices/World_Time.dart';
+
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+
+  void setupWorldTime() async{
+    WorldTime instance=WorldTime(location: 'Berlin',flag:'germany.png',url:'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context,'/home',arguments: {
+      'location':instance.location,
+      'flag':instance.time,
+      'time':instance.time,
+    });
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child:Text('loading'),
+      )
+    );
+  }
+}
